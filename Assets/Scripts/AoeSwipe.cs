3d677c0277a5e7433eda85357508ef31b2e14098
KeeganE596 <kman596@gmail.com>
2019-09-16 21:59:05 +1200

    Adding files for aoe swipe

---
+++ Assets/Scripts/AoeSwipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoeSwipe : MonoBehaviour
+{
+    List<GameObject> gnatts;
+    GameObject[] gs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gnatts = new List<GameObject>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+     {
+         if (col.gameObject.tag == "Gnatt") {
+             gnatts.Add(col.gameObject);
+         }
+     }
+
+    //Return the List of Gnatts that has collided with AOE Swipe
+     public List<GameObject> getGnatts() {
+        return gnatts;
+     }
+
+    //Return if any Gnatts have been hit
+     public bool isGnattsEmpty() {
+        if(gnatts.Count > 0) {
+            return false;
+        }
+        return true;
+     }
+}
