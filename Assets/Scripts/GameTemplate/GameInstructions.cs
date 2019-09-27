9cb4bbbb4b9e897e4ae5c503ced0ef71c745d153
keegane596 <kman596@gmail.com>
2019-09-27 14:02:13 +1200

    added files

---
+++ Assets/Scripts/GameTemplate/GameInstructions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("readInstructionsWait");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator readInstructionsWait()  {
+        Debug.Log("Here");
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Done");
+    }
+}
---
+++ Assets/Scripts/MainMenu/MenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItem : MonoBehaviour
+{
+    public GameObject forScript;
+    MainMenu_TouchDetection mmScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mmScript = forScript.GetComponent<MainMenu_TouchDetection>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D() {
+        mmScript.menuItemDetected(gameObject.name);
+    }
+
+    public void OnTriggerExit2D() {
+        mmScript.menuItemDetected("exit");
+    }
+}
