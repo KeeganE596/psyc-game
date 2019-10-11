0bd8eff526e1e070ae5c0ac7e267627f00a91e2c
Guest User <Guest@wg401-i-04-07.mddn.vuw.ac.nz>
2019-10-11 17:29:07 +1300

    Started breathing game

---
+++ Assets/Scripts/Breathing/Breathing_TouchDetection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathing_TouchDetection : MonoBehaviour
+{
+    public GameObject expandBlob;
+    public GameObject outerCircle;
+
+    bool held;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            expandBlob.transform.localScale += new Vector3(0.013f, 0.013f, 0);
+        }
+        else if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        {
+            if (expandBlob.transform.localScale.x >= 1.5 && expandBlob.transform.localScale.x <= 1.65 && held)
+            {
+                expandBlob.transform.localScale -= new Vector3(0.01f, 0.01f, 0);
+            }
+        }
+        else
+        {
+            if (expandBlob.transform.localScale.x >= 0.2)
+            {
+                expandBlob.transform.localScale = new Vector3(0.1f, 0.1f, 0);
+            }
+
+        }
+    }
+}
+
+//>1.5 <1.65
